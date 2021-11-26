8ef723343cbfff187005f66b874c548e096631d2
ashokkanigiri <ashkanigiri@gmail.com>
2021-11-26 10:25:50 +0530

    code cleanup

--- app/src/main/java/com/ashok/kanigiri/expensemanager/utils/DateUtils.kt
+++ app/src/main/java/com/ashok/kanigiri/expensemanager/utils/DateUtils.kt
@@ -11,10 +11,15 @@ object DateUtils {
     private const val TIME_IN_MILLIS_FORMAT = "yyyy-MM-dd HH:mm:ss"
     private const val DATE_FORMAT_1 = "dd-MM-yyyy"
     private const val DATE_FORMAT_2 = "MMM dd yyyy"
+    private const val DATE_FORMAT_3 = "MMM yyyy"
     //DateFormat.getDateInstance().format(date)  -> FR Locale : 3 nov. 2017
     //val dd  = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date(System.currentTimeMillis()))
 
     fun convertDateToDateFormat(date: Long): String {
         return  SimpleDateFormat(DATE_FORMAT_2).format(date)
+    }
+
+    fun convertDateFormatForAllExpenses(date: Long): String{
+        return  SimpleDateFormat(DATE_FORMAT_3).format(date)
     }
 }
