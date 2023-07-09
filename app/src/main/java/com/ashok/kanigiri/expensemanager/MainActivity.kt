4417522063a1e70b68b5115630566202e2bb9a95
Ashokkanigiri <ashkanigiri@gmail.com>
2023-07-10 02:15:46 +0530

    added in app updates raw code

--- app/src/main/java/com/ashok/kanigiri/expensemanager/MainActivity.kt
+++ app/src/main/java/com/ashok/kanigiri/expensemanager/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.ashok.kanigiri.expensemanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.ashok.kanigiri.expensemanager.databinding.ActivityMainBinding
 import com.ashok.kanigiri.expensemanager.utils.setupWithNavController
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
+import com.google.android.play.core.ktx.isFlexibleUpdateAllowed
+import com.google.android.play.core.ktx.isImmediateUpdateAllowed
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    private lateinit var appUpdateManager: AppUpdateManager
+
+    private val updateType = AppUpdateType.IMMEDIATE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,4 +44,41 @@ class MainActivity : BaseActivity() {
             intent = intent
         )
     }
+
+    private fun checkForUpdates() {
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+
+            val isUpdateAvailable =
+                appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+            val isUpdateAllowed = when (updateType) {
+                AppUpdateType.FLEXIBLE -> appUpdateInfo.isFlexibleUpdateAllowed
+                AppUpdateType.IMMEDIATE -> appUpdateInfo.isImmediateUpdateAllowed
+                else -> false
+            }
+
+            if (isUpdateAllowed && isUpdateAvailable) {
+
+                appUpdateManager.startUpdateFlowForResult(
+                    // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                    appUpdateInfo,
+                    // an activity result launcher registered via registerForActivityResult
+                    appUpdateResultLauncher,
+                    // Or pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
+                    // flexible updates.
+                    AppUpdateOptions.newBuilder(updateType).build())
+            }
+        }
+    }
+
+    val appUpdateResultLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {result ->
+
+        if(result.resultCode != RESULT_OK){
+            Log.d("fnqknf", "something went wrong while updating ... ")
+        }
+
+    }
+
 } 
